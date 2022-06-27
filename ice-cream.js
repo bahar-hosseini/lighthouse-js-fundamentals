311dbb9657c44378a81a8361caa9a78bcd1ea703
Bahar Hosseini <bahar.hosseini@gmail.com>
2022-06-27 16:06:52 -0400

    arrays exercises

---
+++ ice-cream.js
@@ -0,0 +1,8 @@
+let iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+const iceCreamFlavoursCopy = [...iceCreamFlavours,"root beer"];
+console.log(iceCreamFlavoursCopy);
+console.log(iceCreamFlavoursCopy[0]);
+console.log(iceCreamFlavoursCopy[(iceCreamFlavoursCopy.length) - 1]);
+console.log(iceCreamFlavoursCopy.length);
